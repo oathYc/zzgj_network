0bc4f4eaf4084771ee606afab7d466856edcad60
yuchao <yuchao@zhiyuanhui.com>
2022-06-10 11:42:53 +0800

    network package

--- api/controllers/network.go
+++ api/controllers/network.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"time"
 	"zzgj_network/library"
@@ -17,10 +18,14 @@ type NetworkResp struct {
 
 func (*NetworkControl) ImgUpload(ctx *gin.Context) {
 	params := new(NetworkPostReq)
-
 	response := library.NewResponse(ctx)
+
 	if err := library.NewRequest(ctx).Bind(params); nil != err {
 		response.Error(err)
+		return
+	}
+	if params.Img == "" {
+		response.Error(errors.New("上传图片不能为空"))
 		return
 	}
 	time.Sleep(time.Second / 10) // 休眠100毫秒
